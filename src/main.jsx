31a650fef73bdcdba41ae2b4b56fa0d7a11f1bd7
pilinut <pilinut@kali.pilinut.com>
2025-12-02 15:59:22 -0600

    Fix routing and Vite config for custom domain deployment

--- src/main.jsx
+++ src/main.jsx
@@ -14,6 +14,7 @@ createRoot(document.getElementById('root')).render(
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />}>
+          <Route index element={<div></div>} />
           <Route path="events" element={
             <Suspense fallback={<div>Loading…</div>}>
               <Events />
